6787cb255a5f219ac73c494c565239f341997714
Anton Maminov <anton.linux@gmail.com>
2020-02-29 11:53:14 +0200

    add spec for parse invalid point

--- spec/point_spec.cr
+++ spec/point_spec.cr
@@ -10,25 +10,40 @@ describe GeoJSON::Point do
     "coordinates" => position,
   }.to_json
 
-  it "parses json" do
-    point = GeoJSON::Point.from_json(point_json)
+  invalid_point_json = {
+    "type"        => "Point",
+    "coordinates" => [1.0, 1.0, 1.0],
+  }.to_json
 
-    point.should be_a(GeoJSON::Point)
-    point.type.should eq("Point")
-    point.coordinates.should be_a(GeoJSON::Coordinates)
-    point.longitude.should eq(longitude)
-    point.latitude.should eq(latitude)
+  describe "json parser" do
+    it "parses json" do
+      point = GeoJSON::Point.from_json(point_json)
+
+      point.should be_a(GeoJSON::Point)
+      point.type.should eq("Point")
+      point.coordinates.should be_a(GeoJSON::Coordinates)
+      point.longitude.should eq(longitude)
+      point.latitude.should eq(latitude)
+    end
+
+    it "raises an exception if coordinates are invalid" do
+      expect_raises Exception, "GeoJSON::Coordinates must have two coordinates" do
+        GeoJSON::Point.from_json(invalid_point_json)
+      end
+    end
   end
 
-  it "creates object" do
-    point = GeoJSON::Point.new(longitude: longitude, latitude: latitude)
+  describe ".new" do
+    it "creates object" do
+      point = GeoJSON::Point.new(longitude: longitude, latitude: latitude)
 
-    point.should be_a(GeoJSON::Point)
-    point.type.should eq("Point")
-    point.coordinates.should be_a(GeoJSON::Coordinates)
-    point.longitude.should eq(longitude)
-    point.latitude.should eq(latitude)
+      point.should be_a(GeoJSON::Point)
+      point.type.should eq("Point")
+      point.coordinates.should be_a(GeoJSON::Coordinates)
+      point.longitude.should eq(longitude)
+      point.latitude.should eq(latitude)
 
-    point.to_json.should eq(point_json)
+      point.to_json.should eq(point_json)
+    end
   end
 end

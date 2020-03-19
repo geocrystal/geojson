f475c3385fbb19e3b70b6efb581d5604dc4cd4bf
Anton Maminov <anton.linux@gmail.com>
2020-03-19 23:41:16 +0200

    fix some cases for feature collection

--- spec/feature_collection_spec.cr
+++ spec/feature_collection_spec.cr
@@ -1,6 +1,82 @@
 require "./spec_helper"
 
 describe GeoJSON::FeatureCollection do
+  point_coordinates = [102.0, 0.5]
+  line_coordinates = [
+    [102.0, 0.0],
+    [103.0, 1.0],
+    [104.0, 0.0],
+    [105.0, 1.0],
+  ]
+  ring = [
+    [100.0, 0.0],
+    [101.0, 0.0],
+    [101.0, 1.0],
+    [100.0, 1.0],
+    [100.0, 0.0],
+  ]
+  feature_collection_json = {
+    "type"     => "FeatureCollection",
+    "features" => [
+      {
+        "type"     => "Feature",
+        "geometry" => {
+          "type"        => "Point",
+          "coordinates" => point_coordinates,
+        },
+      },
+      {
+        "type"     => "Feature",
+        "geometry" => {
+          "type"      => "LineString",
+          "coordinates": line_coordinates,
+        },
+        "properties" => {
+          "prop0" => "value0",
+          "prop1" => 0.0,
+        },
+      },
+      {
+        "type"     => "Feature",
+        "geometry" => {
+          "type"        => "Polygon",
+          "coordinates" => [ring],
+        },
+        "properties" => {
+          "prop0" => "value0",
+          "prop1" => {
+            "this" => "that",
+          },
+        },
+      },
+    ],
+  }.to_json
+
+  describe "json parser" do
+    it "parses json" do
+      feature_collection = GeoJSON::FeatureCollection.from_json(feature_collection_json)
+
+      feature_collection.should be_a(GeoJSON::FeatureCollection)
+      feature_collection.type.should eq("FeatureCollection")
+      feature_collection.features.should be_a(Array(GeoJSON::Feature))
+      feature_collection.features.size.should eq(3)
+
+      feature1 = feature_collection.features[0]
+      feature1.should be_a(GeoJSON::Feature)
+      feature1.geometry.not_nil!.should be_a(GeoJSON::Point)
+      point = feature1.geometry.not_nil!
+      point.as(GeoJSON::Point).longitude.should eq(102.0)
+
+      feature2 = feature_collection.features[1]
+      feature2.should be_a(GeoJSON::Feature)
+      feature2.geometry.not_nil!.should be_a(GeoJSON::LineString)
+
+      feature3 = feature_collection.features[2]
+      feature3.should be_a(GeoJSON::Feature)
+      feature3.geometry.not_nil!.should be_a(GeoJSON::Polygon)
+    end
+  end
+
   describe ".new" do
     it "initialize Feature with geometry" do
       feature1 = GeoJSON::Feature.new(GeoJSON::Point.new(longitude: 1.0, latitude: 0.1))
--- src/geo_json/object.cr
+++ src/geo_json/object.cr
@@ -17,6 +17,71 @@ module GeoJSON
     # All possible GeoJSON types.
     alias Type = Point | MultiPoint | LineString | MultiLineString | Polygon | MultiPolygon | Feature | FeatureCollection
 
+    alias CoordinatesArray = Array(Float64) | Array(CoordinatesArray)
+
     abstract def type : String
+
+    def self.recursive_array(pull, ary = [] of CoordinatesArray)
+      coordinates = [] of Float64
+
+      pull.read_begin_array
+
+      until pull.kind.end_array?
+        if pull.kind.begin_array?
+          ary << recursive_array(pull)
+        elsif pull.kind.float?
+          coordinates << pull.read_float
+        end
+      end
+
+      pull.read_end_array
+
+      ary.empty? ? coordinates : ary
+    end
+
+    def self.new(pull : JSON::PullParser)
+      pull.read_begin_object
+
+      while !pull.kind.end_object?
+        tmp = pull.read_string
+
+        case tmp
+        when "type"
+          object_type = pull.read_string
+        when "coordinates"
+          coordinates = recursive_array(pull)
+        when "geometry"
+          new(pull)
+        else
+          pull.read_next
+        end
+      end
+
+      pull.read_end_object
+
+      case object_type
+      when "Point"
+        if coordinates
+          return Point.new(coordinates.as(Array(Float64)))
+        end
+      when "LineString"
+        if coordinates
+          coordinates = coordinates.map { |c| c.as(Array(Float64)).map(&.to_f) }
+          return GeoJSON::LineString.new(coordinates)
+        end
+      when "Polygon"
+        if coordinates
+          coordinates = coordinates.map do |ring|
+            ring.as(Array(CoordinatesArray)).map do |point|
+              point.as(Array(Float64)).map(&.to_f)
+            end
+          end
+
+          return GeoJSON::Polygon.new(coordinates)
+        end
+      end
+
+      nil
+    end
   end
 end
